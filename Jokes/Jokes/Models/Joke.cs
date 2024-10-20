07b28429f511764d158fcb6c7851248eebfe3a82
Sujit <sujitbadgujar001@gmail.com>
2024-10-20 23:13:25 +0530

    #Added a Jokes WebApp.

---
+++ Jokes/Jokes/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace Jokes.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string Ans { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
---
+++ Sunday_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sunday_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
