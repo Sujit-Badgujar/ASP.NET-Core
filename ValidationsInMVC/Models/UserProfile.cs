57f98c3e1d08304bd38c0d9ad22e5c5ff5938a71
Sujit <sujitbadgujar001@gmail.com>
2025-01-08 13:40:16 +0530

    Validation of file type in .Net Core MVC App

---
+++ ValidationsInMVC/Models/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using ValidationsInMVC.Utility;
+
+namespace ValidationsInMVC.Models
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+        [AllowedExtensions(new string[] { ".jpg",".jpeg",".png"})]
+        public IFormFile Image { get; set; }
+    }
+}
---
+++ ValidationsInMVC/Utility/AllowedExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationsInMVC.Utility
+{
+    public class AllowedExtensions : ValidationAttribute
+    {
+        private readonly string[] _allowedExtensions;
+        public AllowedExtensions(string[] extension)
+        {
+            _allowedExtensions = extension;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null) 
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_allowedExtensions.Contains(extension.ToLower())) 
+                {
+                    return new ValidationResult($"This image file extension is not allowed!");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
