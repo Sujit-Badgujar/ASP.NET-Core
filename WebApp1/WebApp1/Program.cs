bb1f9cc5bc636af37d387075829f0b1b306e5357
Sujit <sujitbadgujar001@gmail.com>
2025-01-09 00:11:37 +0530

    HTTP Status Code

--- WebApp1/WebApp1/Program.cs
+++ WebApp1/WebApp1/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Sujit! How's it going?");
+//app.MapGet("/", () => "Hello Sujit! How's it going?");
+
+app.Run( async (HttpContext context) =>
+{
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync(" there!");
+
+
+});
 
 app.Run();
