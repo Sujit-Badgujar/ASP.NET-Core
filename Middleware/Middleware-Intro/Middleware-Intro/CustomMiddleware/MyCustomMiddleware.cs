6f5a5f459e39f10a57ee7dbbb299e7c51b7adb58
Sujit <sujitbadgujar001@gmail.com>
2025-01-10 14:29:04 +0530

    CustomMiddleware Example

---
+++ Middleware/Middleware-Intro/Middleware-Intro/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Middleware_Intro.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("CustomMiddleware-1\n");
+
+            await next(context);
+
+            await context.Response.WriteAsync("CustomMiddleware-2\n");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app) 
+        {
+            return app.UseMiddleware<MyCustomMiddleware>();
+            
+        }
+    }
+}
--- Middleware/Middleware-Intro/Middleware-Intro/Program.cs
+++ Middleware/Middleware-Intro/Middleware-Intro/Program.cs
@@ -1,19 +1,34 @@
+using Middleware_Intro.CustomMiddleware;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
+
 var app = builder.Build();
 
-//Middleware in ASP.NET Core:
+//Middleware in ASP.NET Core: using lamda expression
 
-//Middleware 1- using lamda expression
+//Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) => {
 
-    await context.Response.WriteAsync("Hello from Middleware-1 &");
+    await context.Response.WriteAsync("Hello from Middleware-1 &\n");
     await next(context);
 });
 
+//app.Use(async (HttpContext context, RequestDelegate next) => {
+/*app.Use(async (context, next) => {
+
+    await context.Response.WriteAsync("Hello from Middleware-1 &");
+    await next(context); //This is optional only necessary for calling 3rd middleware
+});*/
+
 //Middleware 2
+//app.UseMiddleware<MyCustomMiddleware>();
+
+app.UseMyCustomMiddleware();
+
+//Middleware 3
 app.Run(async (HttpContext context) => {
 
-    await context.Response.WriteAsync("Hello from Middleware-2");
+    await context.Response.WriteAsync("Hello from Middleware-3\n");
 });
 
 app.Run();
