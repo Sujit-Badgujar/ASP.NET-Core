bfaf8afc305de3545efe15190013aa7344acb14d
Sujit <sujitbadgujar001@gmail.com>
2025-01-15 23:50:18 +0530

    Validations Attribuits

--- ModelValidations/ModelValidations/Controllers/HomeController.cs
+++ ModelValidations/ModelValidations/Controllers/HomeController.cs
@@ -8,6 +8,23 @@ namespace ModelValidations.Controllers
         [Route("register")]
         public IActionResult Index(Person person)
         {
+            if (!ModelState.IsValid) 
+            {
+                //For-each loops for model state validations
+                /*List<string> errorList = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errorList.Add(error.ErrorMessage);
+                    }
+                }*/
+
+                //Same using LINQ
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(errors);
+            }
             return Content($"{person}");
         }
     }
--- ModelValidations/ModelValidations/Models/Person.cs
+++ ModelValidations/ModelValidations/Models/Person.cs
@@ -4,12 +4,20 @@ namespace ModelValidations.Models
 {
     public class Person
     {
-        [Required]
-        public  string? PersonName { get; set; }
+        [Required(ErrorMessage = "Dear user, Enter your name!")]
+        [Display(Name = "Person Name")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "{0} should be bet'n {2} to {1} characters long.")]
+        [RegularExpression("^[A-Za-z.]*$", ErrorMessage = "{0} should only contain alphabets!")]
+        public string? PersonName { get; set; }
+
+        [EmailAddress(ErrorMessage ="Improper {0}!")]
         public  string? Email { get; set; }
+        [Phone(ErrorMessage ="Enter proper phone no.!")]
         public  string? Phone { get; set; }
         public  string? Password { get; set; }
         public  string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage ="{0} should be minimum {1} & max: {2}")]
         public  double? Price { get; set; }
 
 
