0efe4d831c1f31c69d55440944b83a3700738da2
Sujit <sujitbadgujar001@gmail.com>
2025-01-15 19:08:24 +0530

    Model Binding & Validations

---
+++ ModelValidations/ModelValidations/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidations.Models;
+
+namespace ModelValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            return Content($"{person}");
+        }
+    }
+}
