2f4e911be8651ba4933a3a82ee50913e4f70b89e
Sujit <sujitbadgujar001@gmail.com>
2025-01-19 23:44:20 +0530

    DependencyInjection-1

---
+++ DI-Example-1/DI-Example-1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DI_Example_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+
+        public HomeController() 
+        {
+            _citiesService = new CitiesService();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService.GetCities();
+
+            return View(cities);
+        }
+    }
+}
---
+++ DI-Example-1/Services/CitiesService.cs
@@ -0,0 +1,22 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> cities;
+
+        public CitiesService() 
+        {
+            cities = new List<string>()
+            {
+              "Sinnar",
+              "Nashik",
+              "Pune"  
+            };
+        }
+
+        public List<string> GetCities() 
+        {
+            return cities;  
+        }
+    }
+}
